5639e13a77d1f23818b3c477225788f2e57e261a
KSG Sounds Macbook Pro <ksgsound@KSGs-MacBook-Pro.local>
2018-09-18 18:24:40 -0500

    final changes

---
+++ assets/firebase/firebase-key.js
@@ -0,0 +1,9 @@
+let config = {
+    apiKey: "AIzaSyCsJcHazdknbNpBbTLENhuMtFvk9b2J50s",
+    authDomain: "trainscheduletth.firebaseapp.com",
+    databaseURL: "https://trainscheduletth.firebaseio.com",
+    projectId: "trainscheduletth",
+    storageBucket: "",
+    messagingSenderId: "200937384615"
+  };
+  firebase.initializeApp(config);
